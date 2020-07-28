f97c52f926b481a4857489ed1978b2d83a4947a9
Dmitry Lebetsky <dmitriy.lebetski@mail.ru>
2020-07-29 00:54:02 +0300

    feat: add data for folk, R&B and country genres of music

--- songbird/src/data/genresData.js
+++ songbird/src/data/genresData.js
@@ -32,13 +32,13 @@ import capitalCitiesSafeAndSoundCover from '../assets/images/songs/pop/capital-c
 import capitalCitiesSafeAndSoundAudio from '../assets/songs/pop/Capital Cities - Safe And Sound.mp3';
 import capitalCitiesSafeAndSoundAudioMinus from '../assets/songs/popMinuses/Capital Cities - Safe And Sound (minus).mp3';
 
-import TomWalkerLeaveALightOnCover from '../assets/images/songs/pop/tom-walker-leave-a-light-on.jpg';
-import TomWalkerLeaveALightOnAudio from '../assets/songs/pop/Tom Walker - Leave a Light On.mp3';
-import TomWalkerLeaveALightOnAudioMinus from '../assets/songs/popMinuses/Tom Walker - Leave a Light On (minus).mp3';
+import tomWalkerLeaveALightOnCover from '../assets/images/songs/pop/tom-walker-leave-a-light-on.jpg';
+import tomWalkerLeaveALightOnAudio from '../assets/songs/pop/Tom Walker - Leave a Light On.mp3';
+import tomWalkerLeaveALightOnAudioMinus from '../assets/songs/popMinuses/Tom Walker - Leave a Light On (minus).mp3';
 
-import RagnBoneManHumanCover from '../assets/songs/pop/rag\'n\'bone-man-human.jpg';
-import RagnBoneManHumanAudio from '../assets/songs/pop/RagnBone Man - Human.mp3';
-import RagnBoneManHumanAudioMinus from '../assets/songs/popMinuses/RagnBone Man - Human (minus).mp3';
+import ragnBoneManHumanCover from '../assets/songs/pop/rag\'n\'bone-man-human.jpg';
+import ragnBoneManHumanAudio from '../assets/songs/pop/RagnBone Man - Human.mp3';
+import ragnBoneManHumanAudioMinus from '../assets/songs/popMinuses/RagnBone Man - Human (minus).mp3';
 
 import siaChandelierCover from '../assets/images/songs/pop/sia-chandelier.jpg';
 import siaChandelierAudio from '../assets/songs/pop/Sia - Chandelier.mp3';
@@ -53,10 +53,89 @@ import theScriptFeatWillIAmHalfOfFameAudio from '../assets/songs/pop/The Script 
 import theScriptFeatWillIAmHalfOfFameAudioMinus from '../assets/songs/popMinuses/The Script feat william - Hall of Fame (minus).mp3';
 
 // Folk
+import folkIcon from '../assets/images/icons/genres-icons/folk-icon.jpg';
+
+import joeyBateyTossACoinToYourWitcherCover from '../assets/images/songs/folk/joey-batey-toss-a-coin-to-your-witcher.jpg';
+import joeyBateyTossACoinToYourWitcherAudio from '../assets/songs/folk/Joey Batey - Toss A Coin To Your Witcher.mp3';
+import joeyBateyTossACoinToYourWitcherAudioMinus from '../assets/songs/folkMinuses/Joey Batey - Toss A Coin To Your Witcher (minus).mp3';
+
+import simonAndGarfunkelSoundOfSilenceCover from '../assets/images/songs/folk/simon-and-garfunkel-soung-of-silence.jpg';
+import simonAndGarfunkelSoundOfSilenceAudio from '../assets/songs/folk/Simon & Garfunkel - Sound of Silence.mp3';
+import simonAndGarfunkelSoundOfSilenceAudioMinus from '../assets/songs/folkMinuses/Simon & Garfunkel - Sound of Silence (minus).mp3';
+
+import edSheeranThePartingGlassCover from '../assets/images/songs/folk/ed-sheeran-the-parting-glass.jpg';
+import edSheeranThePartingGlassAudio from '../assets/songs/folk/Ed Sheeran - The Parting Glass.mp3';
+import edSheeranThePartingGlassAudioMinus from '../assets/songs/folkMinuses/Ed Sheeran - The Parting Glass (minus).mp3';
+
+import americaYouCanDoMagicCover from '../assets/images/songs/folk/america-you-can-do-magic.jpg';
+import americaYouCanDoMagicAudio from '../assets/songs/folk/America - You Can Do Magic.mp3';
+import americaYouCanDoMagicAudioMinus from '../assets/songs/folkMinuses/America - You Can Do Magic (minus).mp3';
+
+import ofMonstersAndMenLittleTalksCover from '../assets/images/songs/folk/of-monsters-and-men-little-talks.jpg';
+import ofMonstersAndMenLittleTalksAudio from '../assets/songs/folk/Of Monsters And Men - Little Talks.mp3';
+import ofMonstersAndMenLittleTalksAudioMinus from '../assets/songs/folkMinuses/Of Monsters And Men - Little Talks (minus).mp3';
+
+import simonGarfunkelOldFriendsBookendsCover from '../assets/images/songs/folk/simon-and-garfunkel-old-friends-bookends.jpg';
+import simonGarfunkelOldFriendsBookendsAudio from '../assets/songs/folk/Simon & Garfunkel - Old Friends Bookends.mp3';
+import simonGarfunkelOldFriendsBookendsAudioMinus from '../assets/songs/folkMinuses/Simon & Garfunkel - Old Friends Bookends (minus).mp3';
+
+// R&B
+import rAndBIcon from '../assets/images/icons/genres-icons/R&B-icon.jpg';
+
+import rayCharlesHitTheRoadJackCover from '../assets/images/songs/R&B/ray-charles-hit-the-road-jack.png';
+import rayCharlesHitTheRoadJackAudio from '../assets/songs/R&B/Ray Charles - Hit The Road Jack.mp3';
+import rayCharlesHitTheRoadJackAudioMinus from '../assets/songs/R&BMinuses/Ray Charles - Hit The Road Jack (minus).mp3';
+
+import arianaGrandeFocusCover from '../assets/images/songs/R&B/ariana-grande-focus.jpg';
+import arianaGrandeFocusAudio from '../assets/songs/R&B/Ariana Grande - Focus.mp3';
+import arianaGrandeFocusAudioMinus from '../assets/songs/R&BMinuses/Ariana Grande - Focus (minus).mp3';
+
+import demiLovatoSoberCover from '../assets/images/songs/R&B/demi-lovato-sober.jpg';
+import demiLovatoSoberAudio from '../assets/songs/R&B/Demi Lovato - Sober.mp3';
+import demiLovatoSoberAudioMinus from '../assets/songs/R&BMinuses/Demi Lovato - Sober (minus).mp3';
+
+import lilPeepXXXTENTACIONFallingDownCover from '../assets/images/songs/R&B/lil-peep-and-xxxtentacion-falling-down.jpg';
+import lilPeepXXXTENTACIONFallingDownAudio from '../assets/songs/R&B/Lil Peep & XXXTENTACION - Falling Down.mp3';
+import lilPeepXXXTENTACIONFallingDownAudioMinus from '../assets/songs/R&BMinuses/Lil Peep & XXXTENTACION - Falling Down (minus).mp3';
+
+import flyboyTarantinoAndCraigXenAndKidwayNowOrNeverCover from '../assets/images/songs/R&B/flyboy-tarantino-and-craig-xen-and-kidway-no-or-never.jpg';
+import flyboyTarantinoAndCraigXenAndKidwayNowOrNeverAudio from '../assets/songs/R&B/Flyboy Tarantino, Craig Xen & Kidway - Now Or Never.mp3';
+import flyboyTarantinoAndCraigXenAndKidwayNowOrNeverAudioMinus from '../assets/songs/R&BMinuses/Flyboy Tarantino, Craig Xen & Kidway - Now Or Never (minus).mp3';
+
+import laurenDaigleThisGirlCover from '../assets/images/songs/R&B/lauren-daigle-this-girl.jpg';
+import laurenDaigleThisGirlAudio from '../assets/songs/R&B/Lauren Daigle - This Girl.mp3';
+import laurenDaigleThisGirlAudioMinus from '../assets/songs/R&BMinuses/Lauren Daigle - This Girl (minus).mp3';
+
+// Country
+import countryIcon from '../assets/images/icons/genres-icons/country-icon.jpg';
+
+import johnnyCashHurtCover from '../assets/images/songs/country/johny-cash-hurt.jpg';
+import johnnyCashHurtAudio from '../assets/songs/country/Johnny Cash - Hurt.mp3';
+import johnnyCashHurtAudioMinus from '../assets/songs/countryMinuses/Johnny Cash - Hurt (minus).mp3';
+
+import mileyCyrusNothingBreaksLikeAHeartCover from '../assets/images/songs/country/miley-cyrus-nothing-breaks-like-a-heart.jpg';
+import mileyCyrusNothingBreaksLikeAHeartAudio from '../assets/songs/country/Mark Ronson - Nothing Breaks Like a Heart.mp3';
+import mileyCyrusNothingBreaksLikeAHeartAudioMinus from '../assets/songs/countryMinuses/Miley Cyrus - Nothing Breaks Like A Heart (minus).mp3';
+
+import beyonceDaddyLessonsCover from '../assets/images/songs/country/beyonce-daddy-lessons.png';
+import beyonceDaddyLessonsAudio from '../assets/songs/country/Beyoncé - Daddy Lessons.mp3';
+import beyonceDaddyLessonsAudioMinus from '../assets/songs/countryMinuses/Beyoncé - Daddy Lessons (minus).mp3';
+
+import theBandPerryIfIDieYoungCover from '../assets/images/songs/country/the-bend-perry-if-i-die-young.jpg';
+import theBandPerryIfIDieYoungAudio from '../assets/songs/country/The Band Perry - If I Die Young.mp3';
+import theBandPerryIfIDieYoungAudioMinus from '../assets/songs/countryMinuses/The Band Perry - If I Die Young (minus).mp3';
+
+import taylorSwiftHauntedCover from '../assets/images/songs/country/taylor-swift-haunted.jpg';
+import taylorSwiftHauntedAudio from '../assets/songs/country/Taylor Swift - Haunted.mp3';
+import taylorSwiftHauntedAudioMinus from '../assets/songs/countryMinuses/Taylor Swift - Haunted (minus).mp3';
+
+import rodneyAtkinsIfYoureGoingThroughHellCover from '../assets/images/songs/country/rodney-atkins-if-youre-going-through-hell.jpg';
+import rodneyAtkinsIfYoureGoingThroughHellAudio from '../assets/songs/country/Rodney Atkins - If You\'re Going Through Hell.mp3';
+import rodneyAtkinsIfYoureGoingThroughHellAudioMinus from '../assets/songs/countryMinuses/Rodney Atkins - If You\'re Going Through Hell (minus).mp3';
 
 const genresData = [
   {
-    title: 'rock',
+    title: 'Rock',
     icon: rockIcon,
     songs: [
       {
@@ -104,7 +183,7 @@ const genresData = [
     ],
   },
   {
-    title: 'pop',
+    title: 'Pop',
     icon: popIcon,
     songs: [
       {
@@ -117,16 +196,16 @@ const genresData = [
       {
         title: 'Tom Walker - Leave a Light On',
         description: '“Leave a Light On” were inspired by a friend of Tom who had been battling severe drug addiction. In the song, Walker encourages his friend to get help. He goes on to tell his friend that if he ever loses his way, he (Walker) “will leave the light on” for him so he can find his way back.',
-        cover: TomWalkerLeaveALightOnCover,
-        audio: TomWalkerLeaveALightOnAudio,
-        audioMinus: TomWalkerLeaveALightOnAudioMinus,
+        cover: tomWalkerLeaveALightOnCover,
+        audio: tomWalkerLeaveALightOnAudio,
+        audioMinus: tomWalkerLeaveALightOnAudioMinus,
       },
       {
         title: 'Rag\'n\'Bone Man - Human',
         description: 'In “Human”, Rag’n’Bone Man is speaking to his mortality.  This is not done in the physical sense, as in the temporalness of his physical body.  Rather he expresses this sentiment along the lines of acknowledging his inherent flaws and inability to resolve certain issues.',
-        cover: RagnBoneManHumanCover,
-        audio: RagnBoneManHumanAudio,
-        audioMinus: RagnBoneManHumanAudioMinus,
+        cover: ragnBoneManHumanCover,
+        audio: ragnBoneManHumanAudio,
+        audioMinus: ragnBoneManHumanAudioMinus,
       },
       {
         title: 'Sia - Chandelier',
@@ -149,6 +228,150 @@ const genresData = [
         audio: theScriptFeatWillIAmHalfOfFameAudio,
         audioMinus: theScriptFeatWillIAmHalfOfFameAudioMinus,
       }
+    ],
+  },
+  {
+    title: 'Folk',
+    icon: folkIcon,
+    songs: [
+      {
+        title: 'Joey Batey - Toss A Coin To Your Witcher',
+        description: 'The song basically describe the events of Episode Two of The Witcher',
+        cover: joeyBateyTossACoinToYourWitcherCover,
+        audio: joeyBateyTossACoinToYourWitcherAudio,
+        audioMinus: joeyBateyTossACoinToYourWitcherAudioMinus,
+      },
+      {
+        title: 'Simon & Garfunkel - Sound of Silence',
+        description: 'The song uses the imagery of light and darkness to show how people\'s ignorance and apathy destroys their ability to communicate even on simple levels.',
+        cover: simonAndGarfunkelSoundOfSilenceCover,
+        audio: simonAndGarfunkelSoundOfSilenceAudio,
+        audioMinus: simonAndGarfunkelSoundOfSilenceAudioMinus,
+      },
+      {
+        title: 'Ed Sheeran – The Parting Glass',
+        description: 'This song is about Irish tradition. When a father, brother, or lifelong friend dies, to go out with a group of other close men and have a drink.',
+        cover: edSheeranThePartingGlassCover,
+        audio: edSheeranThePartingGlassAudio,
+        audioMinus: edSheeranThePartingGlassAudioMinus,
+      },
+      {
+        title: 'America - You Can Do Magic',
+        description: 'This song is about a skeptic who doesn\'t believe in love until he meets a girl who shows him it\'s real.',
+        cover: americaYouCanDoMagicCover,
+        audio: americaYouCanDoMagicAudio,
+        audioMinus: americaYouCanDoMagicAudioMinus,
+      },
+      {
+        title: 'Of Monsters And Men - Little Talks',
+        description: 'This song is about an old couple that have been in love for so many years.',
+        cover: ofMonstersAndMenLittleTalksCover,
+        audio: ofMonstersAndMenLittleTalksAudio,
+        audioMinus: ofMonstersAndMenLittleTalksAudioMinus,
+      },
+      {
+        title: 'Simon & Garfunkel - Old Friends Bookends',
+        description: 'This song is about long-lasting friendship that growing old together.',
+        cover: simonGarfunkelOldFriendsBookendsCover,
+        audio: simonGarfunkelOldFriendsBookendsAudio,
+        audioMinus: simonGarfunkelOldFriendsBookendsAudioMinus,
+      },
+    ],
+  },
+  {
+    title: 'R&B',
+    icon: rAndBIcon,
+    songs: [
+      {
+        title: 'Ray Charles - Hit the Road Jack',
+        description: '“Hit the Road Jack” captures the demands a tired partner makes when they want their significant other to leave for good, and never come back to hurt them. The narrator is so determined to end the relationship that even if her man drags his feet about packing out of their house a bit, she is willing to leave instead.',
+        cover: rayCharlesHitTheRoadJackCover,
+        audio: rayCharlesHitTheRoadJackAudio,
+        audioMinus: rayCharlesHitTheRoadJackAudioMinus,
+      },
+      {
+        title: 'Ariana Grande - Focus',
+        description: 'This song is about Ariana\'s relationship with the world and culture around her.',
+        cover: arianaGrandeFocusCover,
+        audio: arianaGrandeFocusAudio,
+        audioMinus: arianaGrandeFocusAudioMinus,
+      },
+      {
+        title: 'Demi Lovato - Sober',
+        description: 'Demi Lovato is looking back at all the expenses she had to incur because of her addictions. She lost friends who were there from her childhood (Marissa) and she is dying inside (quite literally, due to drugs). She just wants to skip this phase. She wants to imagine this all as a dream and wants to be woken when the cold sweats are gone, the shakes are over and she is back to herself.',
+        cover: demiLovatoSoberCover,
+        audio: demiLovatoSoberAudio,
+        audioMinus: demiLovatoSoberAudioMinus,
+      },
+      {
+        title: 'Lil Peep & XXXTENTACION - Falling Down',
+        description: '“Falling Down” is a posthumous collaboration between the late American rappers Lil Peep and XXXTentacion. This track is a new version of the track “Sunlight on Your Skin”. “Sunlight on Your Skin” was recorded by Lil Peep and American rapper/singer iLoveMakonnen in London shortly before the former’s death in 2017.',
+        cover: lilPeepXXXTENTACIONFallingDownCover,
+        audio: lilPeepXXXTENTACIONFallingDownAudio,
+        audioMinus: lilPeepXXXTENTACIONFallingDownAudioMinus,
+      },
+      {
+        title: 'Flyboy Tarantino, Craig Xen & Kidway - Now Or Never',
+        description: 'In “Now or Never,” Flyboy Tarantino, Craig Xen, and KidWay talk about a person who abandoned them.',
+        cover: flyboyTarantinoAndCraigXenAndKidwayNowOrNeverCover,
+        audio: flyboyTarantinoAndCraigXenAndKidwayNowOrNeverAudio,
+        audioMinus: flyboyTarantinoAndCraigXenAndKidwayNowOrNeverAudioMinus,
+      },
+      {
+        title: 'Lauren Daigle - This Girl',
+        description: 'This song is about lost girl. Finally she realized that God is the only One who can save her and she will never go anywhere from His sight.',
+        cover: laurenDaigleThisGirlCover,
+        audio: laurenDaigleThisGirlAudio,
+        audioMinus: laurenDaigleThisGirlAudioMinus,
+      },
+    ],
+  },
+  {
+    title: 'Country',
+    icon: countryIcon,
+    songs: [
+      {
+        title: 'Johnny Cash - Hurt',
+        description: 'This song, originally written and performed by Nine Inch Nails, is of disputed meaning: is it about self harm, doing heroin, or both?',
+        cover: johnnyCashHurtCover,
+        audio: johnnyCashHurtAudio,
+        audioMinus: johnnyCashHurtAudioMinus,
+      },
+      {
+        title: 'Miley Cyrus - Nothing Breaks Like A Heart',
+        description: 'The song itself is about a toxic relationship coming to an end.',
+        cover: mileyCyrusNothingBreaksLikeAHeartCover,
+        audio: mileyCyrusNothingBreaksLikeAHeartAudio,
+        audioMinus: mileyCyrusNothingBreaksLikeAHeartAudioMinus,
+      },
+      {
+        title: 'Beyoncé - Daddy Lessons',
+        description: 'This song is based on the singer’s recollections of the lessons given to her by her father. It was written before Beyoncé got involved in its composition, so part of it is not necessarily based on her personal experiences.',
+        cover: beyonceDaddyLessonsCover,
+        audio: beyonceDaddyLessonsAudio,
+        audioMinus: beyonceDaddyLessonsAudioMinus,
+      },
+      {
+        title: 'The Band Perry - If I Die Young',
+        description: 'The meaning of the song is simple, it’s about making the most of whatever time you are given, whether it’s 2 years or 20 years.',
+        cover: theBandPerryIfIDieYoungCover,
+        audio: theBandPerryIfIDieYoungAudio,
+        audioMinus: theBandPerryIfIDieYoungAudioMinus,
+      },
+      {
+        title: 'Taylor Swift - Haunted',
+        description: '\'Haunted\' is about the moment that you realize the person you\'re in love with is drifting and fading fast. And you don\'t know what to do, but in that period of time, in that phase of love, where it\'s fading out, time moves so slowly.',
+        cover: taylorSwiftHauntedCover,
+        audio: taylorSwiftHauntedAudio,
+        audioMinus: taylorSwiftHauntedAudioMinus,
+      },
+      {
+        title: 'Rodney Atkins - If Youre Going Through Hell',
+        description: 'This is Rodney Atkins\' signature song, telling the story about the breaking down the walls of life standing in your way. The inspirational story relates to Atkins, whose adoptive parents helped him through a childhood respiratory infection and taught him the value of perseverance.',
+        cover: rodneyAtkinsIfYoureGoingThroughHellCover,
+        audio: rodneyAtkinsIfYoureGoingThroughHellAudio,
+        audioMinus: rodneyAtkinsIfYoureGoingThroughHellAudioMinus,
+      },
     ],
   },
 ]
